613be6590a7a0731ad38072ffd2aa1c4d2aa58de
Alexander Dmitriev <alex.basrike@gmail.com>
2023-07-12 13:21:28 +0300

    worked with video 02-continue-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    console.log('APP rendering');
     return (
         <div className="App">
-            This is APP component
+            <AppTitle/>
             <Rating/>
             <Accordion/>
+            <Rating/>
         </div>
     );
 }
 
 
+function AppTitle() {
+    console.log('App title rendering');
+    return (
+        <>This is APP component</>
+    );
+}
+
 function Rating() {
+    console.log('Star block rendering');
     return (
         <div>
             <Star/>
@@ -26,21 +35,37 @@ function Rating() {
 }
 
 function Star() {
+    console.log('Star element rendering');
     return (
         <div>Star</div>
     );
 }
 
 function Accordion() {
+    console.log('Accordion rendering');
     return (
         <div>
-            <h3>Меню</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+            <AccordionTitle/>
+            <AccordionBody/>
         </div>
+    );
+}
+
+function AccordionTitle() {
+    console.log('AccordionTitle rendering');
+    return (
+        <h3>Меню</h3>
+    );
+}
+
+function AccordionBody() {
+    console.log('AccordionBody rendering');
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
     );
 }
 
