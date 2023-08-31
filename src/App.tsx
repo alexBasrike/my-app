b717fedb9b725d5b1af4db3866f9a2f0dd3affd9
Alexander Dmitriev <alex.basrike@gmail.com>
2023-08-31 21:32:39 +0300

    updated the Star component

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,26 @@ import AppTitle from './components/AppTitle/AppTitle';
 import Rating from './components/Rating/Rating';
 import Accordion from './components/Accordion/Accordion';
 import {OnOff} from "./components/OnOff/OnOff";
+import UncontrollableRating from "./components/UncontrollableRating/UncontrollableRating";
 
 function App() {
     console.log('APP rendering');
     return (
         <div className="App">
             <AppTitle title={"This is APP component"}/>
-            <Accordion titleValue={"Menu"}/>
-            <Accordion titleValue={"Users"}/>
 
-            <Rating value={0}/>
+            {/*<Rating value={0}/>
             <Rating value={1}/>
             <Rating value={2}/>
             <Rating value={3}/>
             <Rating value={4}/>
-            <Rating value={5}/>
+            <Rating value={5}/>*/}
+
+            <Accordion titleValue={"Menu"}/>
 
             <OnOff/>
-            <OnOff/>
+
+            <UncontrollableRating/>
         </div>
     );
 }
